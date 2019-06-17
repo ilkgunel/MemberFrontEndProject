a4f06b19e9c4d6d8c8c5e06a7efd84c129d58acd
ilkgunel <ilkgunel93@gmail.com>
2019-06-17 09:07:35 +0300

    Yeni butonlar eklenmesi

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { MemberService } from 'src/services/member.service';
 import {Router} from '@angular/router';
 import { AuthenticationService } from 'src/services/authentication.service';
+import * as jwt_decode from 'jwt-decode';
+import { Role } from 'src/enum/role';
 
 
 @Component({
@@ -13,14 +15,26 @@ export class MemberListComponent implements AfterViewInit{
     displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email','enabled','password','memberLanguageCode','roleOfMember'];
     public members = [];
     public errorMsg;
+    public isAdmin:boolean = false;
     constructor(
                 private _memberService: MemberService,
                 private _authService: AuthenticationService,
                 private router: Router,
                 ){}
 
+    ngOnInit() {
+      var decoded = jwt_decode(localStorage.getItem('bearerToken'));
+      var currentUserRole = decoded.roles;
+      if(currentUserRole == Role.Admin){
+        console.log("true dönüyoruz!");
+        this.isAdmin = true;
+      } else {
+        console.log("false dönüyoruz!");
+        this.isAdmin = false;
+      }
+    }
+
     ngAfterViewInit() {
-      console.log('ngAfterViewInit içerisindeyiz: '+ localStorage.getItem('bearerToken'));
       this._memberService.getMembers().subscribe(data => this.members = data);
     }
 
