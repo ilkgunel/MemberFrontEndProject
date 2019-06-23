90f0b77d5558e5fbcd18d5e6a6f6eebfd16c227a
ilkgunel <ilkgunel93@gmail.com>
2019-06-23 15:59:26 +0300

    Update Button için ek geliştirme

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -38,13 +38,6 @@ export class MemberListComponent implements AfterViewInit{
       } else {
         this.isAdmin = false;
       }
-
-      if(this.selection.selected.length == 0 || this.selection.selected.length > 1) {
-        this.disableUpdateButton = true;
-      }
-      else if((!this.isAdmin && this.selection.selected[0].roleOfMember.role != currentUserRole)) {
-        this.disableUpdateButton = true;
-      }
     }
 
     ngAfterViewInit() {
@@ -107,4 +100,15 @@ export class MemberListComponent implements AfterViewInit{
     }
   }
 
+  rowClickEvent(row) {
+    this.selection.toggle(row);
+    if(this.selection.selected.length == 0 || this.selection.selected.length > 1) {
+      this.disableUpdateButton = true;
+    }
+    else if((!this.isAdmin && this.selection.selected[0].roleOfMember.role != this.currentUserRole)) {
+      this.disableUpdateButton = true;
+    } else {
+      this.disableUpdateButton = false;
+    }
+  }
 }
